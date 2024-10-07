602d0b7049734bdf8f05df08c7a2d45ff965b9c1
brianna-oregan <brianna.oregan@stu.ocsb.ca>
2024-10-07 00:10:42 +0000

    add FizzBuzz

--- program.py
+++ program.py
@@ -7,7 +7,14 @@
 # End at 32
 
 result = ""
-for myNumber in range(32):
-  result = str(result) + str(myNumber) + "\n"
+for myNumber in range(1, 33): # start at 1, end at 32
+  if myNumber % 15 == 0:
+    result = str(result) + "FizzBuzz\n"
+  elif myNumber % 5 == 0:
+    result = str(result) + "Buzz\n"
+  elif myNumber % 3 == 0:
+    result = str(result) + "Fizz\n"
+  else:
+    result = str(result) + str(myNumber) + "\n"
 
 print(result)
